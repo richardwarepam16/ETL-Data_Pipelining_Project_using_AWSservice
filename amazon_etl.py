3dce2d7dd98528946bd84c0aa38a3ab2612ae969
richardwarepam16 <richardwarepam@gmail.com>
2023-05-07 01:10:02 +0530

    first commit

---
+++ amazon_etl.py
@@ -0,0 +1,82 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import numpy as np
+
+# Function to extract Product Title
+def get_title(soup):
+
+    try:
+        # Outer Tag Object
+        title = soup.find("span", attrs={"id":'productTitle'})
+        
+        # Inner NavigatableString Object
+        title_value = title.text
+
+        # Title as a string value
+        title_string = title_value.strip()
+
+    except AttributeError:
+        title_string = ""
+
+    return title_string
+
+# Function to extract Product Price
+def get_price(soup):
+
+    try:
+        price = soup.find("span", attrs={'id':'price'}).text
+
+    except AttributeError:
+        price = ""
+
+    return price
+
+# Function to extract Product Rating
+def get_rating(soup):
+
+    try:
+        rating = soup.find("i", attrs={'class':'a-icon a-icon-star a-star-4-5'}).string.strip()
+    
+    except AttributeError:
+        try:
+            rating = soup.find("span", attrs={'class':'a-icon-alt'}).string.strip()
+        except:
+            rating = ""	
+
+    return rating
+
+# Function to extract Number of User Reviews
+def get_review_count(soup):
+    try:
+        review_count = soup.find("span", attrs={'id':'acrCustomerReviewText'}).string.strip()
+
+    except AttributeError:
+        review_count = ""	
+
+    return review_count
+
+# Function to extract Availability Status
+def get_availability(soup):
+    try:
+        available = soup.find("div", attrs={'id':'availability'})
+        available = available.find("span").string.strip()
+
+    except AttributeError:
+        available = "Not Available"	
+
+    return available
+
+# Function to extract author name
+def get_author(soup):
+    try:
+        author_elements = soup.find_all('span', {'class': 'author notFaded'})
+        authors = []
+        for author_element in author_elements:
+            author_name = author_element.find('a').text.strip()
+            author_name = author_name.replace('(Author)', '').strip()
+            authors.append(author_name)
+    except:
+        pass
+    return authors
+
